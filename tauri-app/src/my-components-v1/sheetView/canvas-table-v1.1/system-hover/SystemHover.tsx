691007536da64657a0d8a274257e2a3910ed68c3
JoshXie <61863381+JoshXie0809@users.noreply.github.com>
2025-07-07 16:18:40 +0800

    hover 系統完成

--- tauri-app/src/my-components-v1/sheetView/canvas-table-v1.1/system-hover/SystemHover.tsx
+++ tauri-app/src/my-components-v1/sheetView/canvas-table-v1.1/system-hover/SystemHover.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef } from "react";
 import { useSheetView } from "../../SheetView-Context"
+import { TransSystemName } from "../Dirty/DirtyTranslateCellScheduler";
 
 export const SystemHover: React.FC = () => {
   const { containerRef, vcRef } = useSheetView();
@@ -9,8 +10,9 @@ export const SystemHover: React.FC = () => {
   useEffect(() => {
     const container = containerRef.current;
     if (!container) return;
-
     const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    if(!ctx) return;
 
     canvas.style.position = "absolute";
     canvas.style.top = "0px";
@@ -29,23 +31,24 @@ export const SystemHover: React.FC = () => {
     const handleScroll = () => {
       if(!containerRef.current) return;
       if(!canvasRef.current) return;
+      
 
       const container = containerRef.current;
       const canvas = canvasRef.current;
+      const ctx = canvas.getContext("2d");
+      if(!ctx) return;
 
       let ticking = false;
       if (!ticking) {
         ticking = true; // 立即設定為 true，表示已排程一個幀
-
         requestAnimationFrame(() => {
+          ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
           const scrollTop = container.scrollTop;
           const scrollLeft = container.scrollLeft;
           canvas.style.transform = `translate3d(${scrollLeft}px, ${scrollTop}px, 0)`;    
           ticking = false; // 所有更新完成後，將 ticking 設為 false
         });
       }
-
-      
     }
 
     container.addEventListener("scroll", handleScroll);
@@ -64,26 +67,55 @@ export const SystemHover: React.FC = () => {
   useEffect(() => {
     const container = containerRef.current;
     if (!container) return;
+    const canvas = canvasRef.current;
+    if(!canvas) return;
+    const ctx = canvas.getContext("2d");
+    if(!ctx) return;
+    const vc = vcRef.current;
+    if(!vc) return;
 
     const handleMouseMove = (e: MouseEvent) => {
+      const container = containerRef.current;
+      if (!container) return;
+      const canvas = canvasRef.current;
+      if(!canvas) return;
+      const ctx = canvas.getContext("2d");
+      if(!ctx) return;
+      const vc = vcRef.current;
+      if(!vc) return;
+
+      const scrollTop = container.scrollTop;
+      const scrollLeft = container.scrollLeft;
+      const rowHeight = vc.cellHeight;
+      const cellWidth = vc.cellWidth;
+
       // 取得滑鼠目前指到的畫面座標
       const { clientX, clientY } = e;
 
       // 查出目前滑鼠底下的元素
       const el = document.elementFromPoint(clientX, clientY) as HTMLElement | null;
-      const target = findTransSystemElement(el);
+      let ticking = false;
 
+      if(!ticking) {
+        ticking = true;
+        const target = findTransSystemElement(el);
 
-      if (target) {
-        const system = target.dataset.transSystem;
-        const transX = target.dataset.transX;
-        const transY = target.dataset.transY;
+        requestAnimationFrame(() => {
+          // if (target) {
+          //   const system = target.dataset.transSystem;
+          //   const transX = target.dataset.transX;
+          //   const transY = target.dataset.transY;
 
-        console.log(`滑鼠指到 ${system} 區塊，位置為 (${transX}, ${transY})`);
-      } else {
-        console.log("未指向任何 trans-system 元素");
+          //   console.log(`滑鼠指到 ${system} 區塊，位置為 (${transX}, ${transY})`);
+          // } else {
+          //   console.log("未指向任何 trans-system 元素");
+          // }
+
+          drawCell(target, ctx, scrollTop, scrollLeft, rowHeight, cellWidth);
+
+          ticking = false;
+        })
       }
-
     };
 
     container.addEventListener("mousemove", handleMouseMove);
@@ -106,3 +138,93 @@ function findTransSystemElement(el: HTMLElement | null): HTMLElement | null {
   }
   return null;
 }
+
+function drawCell(
+  target: HTMLElement | null, 
+  ctx: CanvasRenderingContext2D, 
+  scrollTop: number, scrollLeft: number,
+  rowHeight: number, cellWidth: number,
+) {
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  ctx.fillStyle = "rgba(194, 221, 241, 0.4)";
+
+  if(target) {
+    const transSystemName = target.dataset.transSystem as TransSystemName;
+    const transX = Number(target.dataset.transX);
+    const transY = Number(target.dataset.transY);
+
+    let offsetX = 0;
+    let offsetY = 0;
+    let x = 0;
+    let w = 0;
+    let y = 0;
+    let h = 0;
+
+    switch(transSystemName) {
+      case "cells":
+        offsetX = cellWidth;
+        offsetY = rowHeight;
+        x = transX + offsetX - scrollLeft;
+        w = cellWidth;
+
+        if(x < cellWidth) {
+          w = x;
+          x = cellWidth;
+        }
+          
+        y = transY + offsetY - scrollTop;
+        h = rowHeight;
+
+        if(y < rowHeight) {
+          h = y;
+          y = rowHeight;
+        }
+
+        ctx.fillRect(x, y, w, h);
+        break;
+      
+      case "column-header":
+        offsetX = cellWidth;
+        offsetY = scrollTop;
+
+        x = transX + offsetX - scrollLeft;
+        w = cellWidth;
+        y = transY + offsetY - scrollTop;
+        h = rowHeight;
+
+        if(x < cellWidth) {
+          w = x;
+          x = cellWidth;
+        }
+
+        ctx.fillRect(x, y, w, h);
+        break;
+      
+      case "row-header":
+        offsetX = scrollLeft;
+        offsetY = rowHeight;
+
+        x = transX + offsetX - scrollLeft;
+        w = cellWidth;
+        y = transY + offsetY - scrollTop;
+        h = rowHeight;
+
+        if(y < rowHeight) {
+          h = y;
+          y = rowHeight;
+        }
+
+        ctx.fillRect(x, y, w, h);
+        break;
+
+      case "tlc":
+        offsetX = 0;
+        offsetY = 0;
+        break;
+
+      default:
+        break;
+    }
+  }
+}
+
